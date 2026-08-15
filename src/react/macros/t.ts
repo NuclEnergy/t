2358bc6c2e1c156df3360e0dd499febba28a59f4
Jiaxiang Zhang <nuclefpv@nuclefpv.com>
2026-08-16 00:53:45 +0200

    fix(react): return rendered translation nodes

--- src/react/macros/t.ts
+++ src/react/macros/t.ts
@@ -1,10 +1,13 @@
 import { renderTemplate } from "../render";
 import { VarsFor } from "../types";
 
-export const t = <K extends string>(key: K, ...args: VarsFor<K>): string => {
+export const t = <K extends string>(
+  key: K,
+  ...args: VarsFor<K>
+): React.ReactNode => {
   const vars = args[0];
   if (vars) {
-    return `[${renderTemplate(key, vars)}]`;
+    return renderTemplate(`[${key}]`, vars);
   }
   return `[${key}]`;
 };
